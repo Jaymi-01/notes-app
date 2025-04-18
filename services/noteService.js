e1b15012b209a9486f525f6cfd90bfad191a401d
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-04-18 21:12:52 +0100

    added add new notes ftn

--- services/noteService.js
+++ services/noteService.js
@@ -5,15 +5,38 @@ import { ID } from "react-native-appwrite";
 const dbId = process.env.EXPO_PUBLIC_APPWRITE_DB_ID;
 const colId = process.env.EXPO_PUBLIC_APPWRITE_COL_NOTES_ID;
 
-const noteService ={
-    //Get Notes
-    async getNotes(){
-        const response = await databaseService.listDocuments(dbId, colId);
-        if (response.error){
-            return{error: response.error};
-        }
-        return {data: response}
-    },
+const noteService = {
+  //Get Notes
+  async getNotes() {
+    const response = await databaseService.listDocuments(dbId, colId);
+    if (response.error) {
+      return { error: response.error };
+    }
+    return { data: response };
+  },
+  //Add New Note
+  async addNote() {
+    if (!text) {
+      return { error: "Note text cannot be empty, baka!" };
+    }
+
+    const data = {
+      text: text,
+      createdAt: new Date().toISOString(),
+    };
+
+    const response = await databaseService.createDocument(
+      dbId,
+      colId,
+      data,
+      ID.unique()
+    );
+
+    if (response?.error) {
+      return { error: response.error };
+    }
+    return { data: response };
+  },
 };
 
 export default noteService;

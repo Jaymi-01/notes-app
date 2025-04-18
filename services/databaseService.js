cb8cf30af5da258035fdd30d2918bc3bc9fc4934
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-04-18 21:04:38 +0100

    added a create document ftn

--- services/databaseService.js
+++ services/databaseService.js
@@ -11,6 +11,18 @@ const databaseService = {
       return { error: error.message };
     }
   },
+
+  // Create Docs
+  async createDocument(dbId, colId, data, id = null){
+    try {
+      return await database.createDocument(dbId, colId, id || undefined, data)
+    } catch (error) {
+      console.error('Error Creating Document', error.message);
+      return{
+        error: error.message,
+      }
+    }
+  },
 };
 
 export default databaseService;
